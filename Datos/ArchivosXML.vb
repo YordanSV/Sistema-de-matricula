63fa1ee2c13ce3c1f8c09eaa8435bdb19b2af326
Yordan Danilo Salazar Villalobos <yordan10_pv@hotmail.com>
2023-06-28 19:48:35 -0600

    Ya se utiliza el XML  (<carrera><semestre><cursos>)

--- Datos/ArchivosXML.vb
+++ Datos/ArchivosXML.vb
@@ -3,14 +3,21 @@ Imports System.IO
 Imports System.Xml
 
 
-Public Sub Grabar(ByVal route As String, xmlData As MemoryStream)
-    Dim fileXML As FileStream = New FileStream(route, FileMode.Create, FileAccess.Write)
-    xmlData.WriteTo(fileXML)
-
-End Sub
 
 
 Public Class ArchivosXML
+
+
+
+    'xmlData es la informacion del xml que se desea escribir
+    Public Sub Grabar(ByVal route As String, xmlData As MemoryStream)
+        Dim fileXML As FileStream = New FileStream(route, FileMode.Create, FileAccess.Write)
+        xmlData.WriteTo(fileXML)
+        fileXML.Close()
+        xmlData.Close()
+
+    End Sub
+
     Public Function read(ByVal route As String) As XmlDocument
         Dim xmlArchivo As New XmlDocument
         xmlArchivo.Load(route)
